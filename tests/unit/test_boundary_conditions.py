e8abdbf67de0756227d77b79f43cc0933c7b641e
Martin Alnaes <martinal@simula.no>
2013-04-02 17:37:41 +0200

    Copy boundary_conditions.py tests into a file test_boundary_conditions.py following the proper structure.

---
+++ tests/unit/test_boundary_conditions.py
@@ -0,0 +1,33 @@
+"""
+Tests of the custom boundary condition functionality in headflow.
+"""
+
+import unittest
+
+from dolfin import Function, VectorFunctionSpace, Mesh, Expression, DirichletBC
+from headflow import Pouseille, Womersley
+       
+class TestBoundaryConditions(unittest.TestCase):
+    def setUp(self):
+        self.mesh = Mesh("cylinder_4k.xml.gz")
+        x = [0.0, 0.2, 0.4, 0.6, 0.8]
+        y = [1, 5, 3, 2, 1]
+        self.coeffs = zip(x,y)
+
+        self.V = VectorFunctionSpace(self.mesh, "CG", 1)
+        self.u = Function(self.V)
+
+    def _test_bcs(self, bcs):
+        # TODO: Expand
+        for i, bc in enumerate(bcs):
+            assert(isinstance(bc, Expression))
+            dbc = DirichletBC(V.sub(i), bc, 1)
+            dbc.apply(u.vector())
+
+    def test_pouseille(self):
+        bcs = [Pouseille(self.coeffs, self.mesh, 1)]
+        self._test_bcs(bcs)
+
+    def test_womersley(self):
+        bcs = [Womersley(self.coeffs, self.mesh, 1, 4.0)]
+        self._test_bcs(bcs)
