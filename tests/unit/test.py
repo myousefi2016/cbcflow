8387befdf6b97bcc5682e9898e9ced1625ac7fc4
Martin Alnaes <martinal@simula.no>
2013-04-02 17:57:52 +0200

    Refactor test.py files into smaller functions with comments to make it clearer what happens.

--- tests/unit/test.py
+++ tests/unit/test.py
@@ -1,28 +1,52 @@
 #!/usr/bin/env python
 import io, os, sys, unittest, glob
 
-def run(modulename, modulepath, verbosity):
-    if modulepath is not None:
-        # Insert path, intended to import local module instead of installed one
+def inject_python_path(modulepath):
+    "Insert path into global python path, intended to import local module instead of installed one."
+    if modulepath:
         sys.path.insert(0, modulepath)
+
+def import_module_under_test(modulename):
+    "Import module under test and print some info about it."
     mod = __import__(modulename)
     print("Running tests with %s version %s, date %s, imported from\n%s" % (
             modulename, mod.__version__, mod.__date__, mod.__file__))
+    return mod
 
-    # Running tests from all test_foo.py files
+def find_tests_in_current_dir():
+    "Find test_*.py in current directory and return basenames."
     testpattern = "test_*.py"
     tests = sorted(os.path.basename(f).replace(".py", "") for f in glob.glob(testpattern))
+    return tests
 
-    # Setup unittest 
+def load_test_suite(tests):
+    "Build unittest suite from list of importable module names."
     loader = unittest.TestLoader()
     fullsuite = unittest.TestSuite()
     for case in tests:
         casemodule = __import__(case)
         casesuite = loader.loadTestsFromModule(casemodule)
         fullsuite.addTests(casesuite)
-    unittest.TextTestRunner(verbosity=verbosity).run(fullsuite)
+    return fullsuite
+
+def run(modulename, modulepath, verbosity):
+    "Import and run tests."
+    # Import module
+    inject_python_path(modulepath)
+    mod = import_module_under_test(modulename)
+
+    # Get tests
+    tests = find_tests_in_current_dir()
+    fullsuite = load_test_suite(tests)
+
+    # Execute unittest framework
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    runner.run(fullsuite)
 
 if __name__ == "__main__":
+    args = sys.argv[1:]
+    # TODO: Figure out a clean way to run both single and all tests locally or globally
     run(modulename="headflow",
         modulepath="../../site-packages",
         verbosity=2)
+
