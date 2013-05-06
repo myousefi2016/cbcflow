8497672151a1fa164790ff7140f6efc634e6dc10
Martin Sandve Alnæs <martinal@simula.no>
2013-05-06 13:25:05 +0200

    Use LU solver for pressure, CG fails.

--- demo/pipe/run.py
+++ demo/pipe/run.py
@@ -13,13 +13,16 @@ from headflow import *
 from headflow.dol import *
 set_log_level(100)
 
+from headflow.core.utils import get_memory_usage
+print "Memory usage at top of program:", get_memory_usage()
+
 
 # Configure scheme
 spd = ParamDict(
     u_degree=1,
     #solver_p=("gmres", "default"),
-    #solver_p=("lu", "default"),
-    solver_p=("cg", "default"),
+    solver_p=("lu", "default"),
+    #solver_p=("cg", "default"),
     #solver_p=("gmres", "ilu"),
     #solver_p=("cg", "amg"),
     #solver_p=("gmres", "amg"),
@@ -34,7 +37,7 @@ from pipe import Pipe
 ppd = ParamDict(
     #dt = 1e-3,
     #T  = 1e-3 * 100,
-    num_periods=0.002,#3.0,
+    num_periods=0.002, #3.0,
     )
 problem = Pipe(ppd)
 
@@ -58,6 +61,7 @@ ppfield_pd = ParamDict(
 #postprocessor.add_fields([wss, velocity, pressure])
 #postprocessor.add_fields([velocity, pressure])
 
+#parameters["adjoint"]["test_derivative"] = True
 
 # Configure solver
 npd = ParamDict(
@@ -77,11 +81,11 @@ if 0 and enable_annotation:
 
 
 # Optimization
-def update_model_eval(*args):
-    print "update_model_eval: ", args
+def update_model_eval(j, m):
+    pass #print "update_model_eval: ", args
 
 def update_derivative_eval(*args):
-    print "update_derivative_eval: ", args
+    pass #print "update_derivative_eval: ", args
 
 if 1 and enable_annotation:
     # Fetch some stuff from scheme namespace
@@ -106,13 +110,13 @@ if 1 and enable_annotation:
 
     # Try to compute gradient
     if 0:
-        m = p_out_coeffs[0]
-        Jred = ReducedFunctional(J, ScalarParameter(m))
-        Jm = Jred(m)
-        seed = 1.0
+        #m = InitialConditionParameter(u0[0])
+        m = ScalarParameter(p_out_coeffs[0])
+        Jred = ReducedFunctional(J, m)
         dJdm = compute_gradient(J, m, forget=False)
-        res = dolfin_adjoint.taylor_test(Jred, m, Jm, dJdm)#, seed=seed)
-        print "taylor test result =", res
+        #Jm = Jred(m)
+        #res = dolfin_adjoint.taylor_test(Jred, m, Jm, dJdm)#, seed=0.001)
+        #print "taylor test result =", res
     if 0:
         Jred = ReducedFunctional(J, m)
         Jm = Jred([u0c for u0c in u0] + [p_coeff for p_coeff in p_out_coeffs])
@@ -139,3 +143,7 @@ if 1 and enable_annotation:
                                  eval_cb=update_model_eval,
                                  derivative_cb=update_derivative_eval)
         m_opt = minimize(Jred, options={"disp":True}) # bounds=bounds, tol=1e-6,
+
+
+from headflow.core.utils import get_memory_usage
+print "Memory usage at end of program:", get_memory_usage()
