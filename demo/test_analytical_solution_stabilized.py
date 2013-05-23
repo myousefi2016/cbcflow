61de88a09a833bdbc6e6bd91151c73317ed0ea4c
Kent-Andre Mardal <kent-and@simula.no>
2013-05-23 13:29:05 +0200

    update test to run over several schemes, mu, N, and dt

--- demo/test_analytical_solution_stabilized.py
+++ demo/test_analytical_solution_stabilized.py
@@ -4,65 +4,65 @@ import sys; sys.path.insert(0,"../site-packages")
 from headflow import *
 import beltrami
 
-params = beltrami.Beltrami.default_user_params()
-analytical_solution = beltrami.Beltrami.analytical_solution
-
-
-ppfield_pd = ParamDict(
-    saveparams=ParamDict(
-        save=True,
-        ),
-    timeparams=ParamDict(
-        step_frequency=10,
-        )
-    )
-analyzer3 = WSS(params=ppfield_pd)
-
-
-error_data = {}
 Ns = [2, 4, 8]
 dts = [0.1, 0.05, 0.025]
 schemes = [IPCS(None), IPCS_Stable(None), IPCS_Stabilized(None), SegregatedIPCS(None)] 
-for scheme in schemes: 
-    for N in Ns: 
-	for dt in dts: 
-	    params["N"] = N 
-	    params["dt"] = dt 
+schemes = [IPCS(None), IPCS_Stable(None), IPCS_Stabilized(None), SegregatedIPCS(None)] 
 
-	    analyzer1 = AnalyticalSolutionAnalyzer()
-	    analyzer1.params["saveparams"]["save"] = True
-	    analyzer2 = EnergyAnalyzer()
-	    analyzer2.params["saveparams"]["save"] = True
+ppfield_pd = ParamDict(
+    saveparams=ParamDict(
+	save=True,
+	),
+    timeparams=ParamDict(
+	step_frequency=10,
+	)
+    )
+analyzer3 = WSS(params=ppfield_pd)
 
-	    pp = NSPostProcessor({"casedir":"results/beltrami_ipcs_segregated/N=%d/dt=%e" % (N,dt)})
-	    pp.add_field(analyzer1)
-	    pp.add_field(analyzer2)
-	    pp.add_field(analyzer3)
+for mu in [1.0, 1.0e-3]: 
+    params = beltrami.Beltrami.default_user_params()
+    params["mu"] = mu 
 
-	    p = beltrami.Beltrami(params)
-	    nssolver = NSSolver(p, scheme, pp)  
-	    nssolver.solve()
+    analytical_solution = beltrami.Beltrami.analytical_solution
 
-	    error_data[(N, dt)] = analyzer1.get_data()
-    #        print error_data[(N, dt)]["data"]["u1"] 
-
-
-    print ""
-    print ""
-    print "scheme ", scheme 
-    for x in ["u0", "u1", "u2", "p"]:
-	print ""
-	print ""
-	print x 
-	print "dt ", 
-	for dt in dts: 
-	    print dt,  
+    for scheme in schemes: 
+	error_data = {}
 	for N in Ns: 
-	    print "\nN ", N,  
 	    for dt in dts: 
-		print " %2.2e " % error_data[(N, dt)]["data"][x],  
+		params["N"] = N 
+		params["dt"] = dt 
 
-	 
-    print ""
-    print ""
+		analyzer1 = AnalyticalSolutionAnalyzer()
+		analyzer1.params["saveparams"]["save"] = True
+		analyzer2 = EnergyAnalyzer()
+		analyzer2.params["saveparams"]["save"] = True
+
+		pp = NSPostProcessor({"casedir":"results/beltrami_ipcs_segregated/N=%d/dt=%e" % (N,dt)})
+		pp.add_field(analyzer1)
+		pp.add_field(analyzer2)
+		pp.add_field(analyzer3)
+
+		p = beltrami.Beltrami(params)
+		nssolver = NSSolver(p, scheme, pp)  
+		nssolver.solve()
+
+		error_data[(N, dt)] = analyzer1.get_data()
+
+	print ""
+	print ""
+	print "scheme ", scheme, " mu ", mu 
+	for x in ["u0", "u1", "u2", "p"]:
+	    print ""
+	    print ""
+	    print x 
+	    print "dt ", 
+	    for dt in dts: 
+		print dt,  
+	    for N in Ns: 
+		print "\nN ", N,  
+		for dt in dts: 
+		    print " %2.2e " % error_data[(N, dt)]["data"][x],  
+	     
+	print ""
+	print ""
 
