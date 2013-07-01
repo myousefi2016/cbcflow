0fc15b1b19c2035b0997821d7555873ab3c9c770
Martin Sandve Alnæs <martinal@simula.no>
2013-07-01 13:53:59 +0200

    Add parameters u_degree and p_degree to all schemes parameters.
    I find it better to have this information in the source code when looking at a scheme.
    NB! Defaulting to P1-P1 in IPCS schemes, P2-P1 in coupled schemes.

--- tests/unit/test_nssolver.py
+++ tests/unit/test_nssolver.py
@@ -28,7 +28,7 @@ class MockPostProcessor(NSPostProcessor):
 
 class MockScheme(NSScheme):
     def __init__(self, params):
-        NSScheme.__init__(self, params, segregated=False)
+        NSScheme.__init__(self, params)
 
     @classmethod
     def default_user_params(cls):
