c56642da7ac1af8ad192814406cc75f23fb0f7fb
Martin Sandve Alnæs <martinal@simula.no>
2013-07-30 19:32:21 +0200

    Import make_pouseille_bcs and make_womersley_bcs from headflow and slightly improve unit tests to check for nonzero result. Need tests for actually correct values!!

--- tests/unit/test_boundary_conditions.py
+++ tests/unit/test_boundary_conditions.py
@@ -1,35 +1,54 @@
 #!/usr/bin/env python
 """
 Tests of the custom boundary condition functionality in headflow.
+
+FIXME: This is a prime candidate for better unit tests!!!
 """
 
 import unittest
 
-from headflow import Pouseille, Womersley
-from headflow.dol import Function, VectorFunctionSpace, Mesh, Expression, DirichletBC
+from headflow import make_pouseille_bcs, make_womersley_bcs
+from headflow.dol import Function, VectorFunctionSpace, Mesh, MeshFunction, Expression, DirichletBC
 
 class TestBoundaryConditions(unittest.TestCase):
     def setUp(self):
+        # Geometry
         self.mesh = Mesh("data/cylinder_4k.xml.gz")
-        x = [0.0, 0.2, 0.4, 0.6, 0.8]
-        y = [1, 5, 3, 2, 1]
+        self.facet_domains = MeshFunction("size_t", self.mesh, 3-1, self.mesh.domains())
+        self.indicator = 1
+
+        # Temporal profile coefficients
+        x = [0.0, 0.2, 0.3, 0.4, 0.8]
+        y = [1.0, 5.0, 3.0, 2.0, 1.0]
         self.coeffs = zip(x,y)
 
+        # Function space and function
         self.V = VectorFunctionSpace(self.mesh, "CG", 1)
         self.u = Function(self.V)
 
-    def _test_bcs(self, bcs):
-        # TODO: Expand
-        for i, bc in enumerate(bcs):
+    def test_womersley(self):
+        nu = 4.0
+        expressions = make_womersley_bcs(self.coeffs, self.mesh, self.indicator, nu, None, self.facet_domains)
+
+        # Test that expressions are Expressions and can be applied as BCs providing nonzero change to rhs vector
+        self.u.vector().zero()
+        for i, bc in enumerate(expressions):
             self.assertTrue(isinstance(bc, Expression))
             dbc = DirichletBC(self.V.sub(i), bc, 1)
             dbc.apply(self.u.vector())
+        self.assertGreater(self.u.vector().norm('l2'), 0.0)
+
+        # FIXME: Now test that the values are correct!
 
     def test_pouseille(self):
-        bcs = Pouseille(self.coeffs, self.mesh, 1)
-        self._test_bcs(bcs)
+        expressions = make_pouseille_bcs(self.coeffs, self.mesh, self.indicator, None, self.facet_domains)
 
-    def test_womersley(self):
-        bcs = Womersley(self.coeffs, self.mesh, 1, 4.0)
-        self._test_bcs(bcs)
+        # Test that expressions are Expressions and can be applied as BCs providing nonzero change to rhs vector
+        self.u.vector().zero()
+        for i, bc in enumerate(expressions):
+            self.assertTrue(isinstance(bc, Expression))
+            dbc = DirichletBC(self.V.sub(i), bc, 1)
+            dbc.apply(self.u.vector())
+        self.assertGreater(self.u.vector().norm('l2'), 0.0)
 
+        # FIXME: Now test that the values are correct!
