df615fe4d5cebe3d4d0a0d8f7e3ac6fe03dd2543
Øyvind Evju <oyvinev@simula.no>
2014-01-31 15:59:41 +0100

    Changed space for calculation of StreamFunction. Use direct solver.

--- cbcflow/fields/basic/StreamFunction.py
+++ cbcflow/fields/basic/StreamFunction.py
@@ -15,23 +15,23 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with CBCFLOW. If not, see <http://www.gnu.org/licenses/>.
 from ..bases.PPField import PPField
-from dolfin import TrialFunction, TestFunction, dot, grad, DirichletBC, DomainBoundary, dx, Constant, assemble, KrylovSolver, Vector, Function
+from dolfin import TrialFunction, TestFunction, dot, grad, DirichletBC, DomainBoundary, dx, Constant, assemble, KrylovSolver, Vector, Function, solve
 
 class StreamFunction(PPField):
     def before_first_compute(self, pp, spaces, problem):
         u = pp.get("Velocity")
         assert len(u) == 2, "Can only compute stream function for 2D problems"
-        V = spaces.DU0
+        V = spaces.U
         psi = TrialFunction(V)
         self.q = TestFunction(V)
         a = dot(grad(psi), grad(self.q))*dx()
         
         
         self.bc = DirichletBC(V, Constant(0), DomainBoundary())
-        A = assemble(a)
+        self.A = assemble(a)
         self.L = Vector()
-        self.bc.apply(A)
-        self.solver = KrylovSolver(A, "cg")
+        self.bc.apply(self.A)
+        #self.solver = KrylovSolver(A, "cg")
         self.psi = Function(V)
         
 
@@ -40,7 +40,8 @@ class StreamFunction(PPField):
         assemble(dot(u[1].dx(0)-u[0].dx(1), self.q)*dx(), tensor=self.L)
         self.bc.apply(self.L)
         
-        self.solver.solve(self.psi.vector(), self.L)
+        #self.solver.solve(self.psi.vector(), self.L)
+        solve(self.A, self.psi.vector(), self.L)
         
         return self.psi
         
