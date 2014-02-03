bd7e04d7d8552c6c7136cbcb4e5bf4e3dcc16e80
Øyvind Evju <oyvinev@simula.no>
2014-02-03 16:43:28 +0100

    Added restart demo

---
+++ demo/undocumented/Restart/restart.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.insert(0, '../../../demo/undocumented/Beltrami')
+
+import pickle
+
+from cbcflow import *
+from dolfin import *
+
+from beltrami import Beltrami
+
+def play():
+    problem = Beltrami(dict(dt=1e-2, T=1.0))
+    scheme = IPCS()
+    
+    # Need to save velocity and pressure for restart to work
+    fields = [
+        Velocity(dict(save=True, save_as="xml",stride_timestep=5)),
+        Pressure(dict(save=True, stride_timestep=10)),
+        L2norm("Velocity", dict(save=True, stride_timestep=2))
+    ]
+    
+    postprocessor = NSPostProcessor(dict(casedir='results'))
+    
+    postprocessor.add_fields(fields)
+    
+    solver = NSSolver(problem, scheme, postprocessor)
+    solver.solve()
+
+
+def restart():
+    # Load params, to reuse
+    params = pickle.load(open('results/params.pickle', 'r'))
+    
+    problem = Beltrami(params.problem)
+    problem.params.T = 2.0
+    scheme = IPCS_Stable(params.scheme)
+    
+    fields = [
+        Velocity(dict(save=True)),
+        Pressure(dict(save=True)),
+        WSS(dict(save=True)),
+    ]
+    
+    postprocessor = NSPostProcessor(dict(casedir='results'))
+    postprocessor.add_fields(fields)
+    
+    # Set restart data
+    solver_params = params.solver
+    solver_params["restart"] = True
+    solver_params["restart_time"] = 0.5
+        
+    solver = NSSolver(problem, scheme, postprocessor, params=solver_params)
+    solver.solve()
+    
+if __name__ == '__main__':
+    play()
+    restart()
