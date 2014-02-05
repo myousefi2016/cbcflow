be533d5520fc5326747f47c383fec23bf1a1d5f5
Martin Sandve Alnæs <martinal@simula.no>
2014-02-05 16:43:52 +0100

    Some minor refactoring in new tests.

--- tests/validation_pytest/conftest.py
+++ tests/validation_pytest/conftest.py
@@ -1,9 +1,9 @@
+
 from cbcflow import *
 import sys
 import pytest
-import itertools
-from collections import defaultdict
 
+# FIXME: Assuming run from ./, that's not compatible with running all tests from tests/
 sys.path.insert(0, "../../demo/undocumented/Poiseuille2D")
 sys.path.insert(0, "../../demo/undocumented/Poiseuille3D")
 sys.path.insert(0, "../../demo/undocumented/Womersley2D")
@@ -29,13 +29,13 @@ def parse_parameterized(option, opt_str, value, parser):
         if arg[0] == '-':
             break
         nargs += 1
-        
+
         # Create new key if this is not recognized as a parameter
         if "=" not in arg:
             key = arg
             parameterized[key] = ParamDict()
             continue
-        
+
         # Add parameters
         param, value = arg.split('=')
         if value.isdigit():
@@ -53,9 +53,10 @@ def parse_parameterized(option, opt_str, value, parser):
 def pytest_addoption(parser):
     parser.addoption("--type", action="store", default="changed", 
         choices=["changed", "update"], dest="type", help="check if reference has changed, or update reference")
+
     parser.addoption("--testsize", action="store", default="all",
         choices=["all", "fast", "slow", "slowest", "debug"], dest="testsize", help="run all, slow or fast test")
-    
+
     # TODO: Add option for validation mode
     #parser.addoption("--mode", action="store", default="regression", choices=["regression", "validation"],
     #                 dest="mode", help="Run regression test or generate validation figures")
@@ -63,60 +64,59 @@ def pytest_addoption(parser):
     parser.addoption("--schemes", action="callback", callback=parse_parameterized, dest="schemes")
     parser.addoption("--problems", action="callback", callback=parse_parameterized, dest="problems")
     
-def updated_paramdict(params, add_dict):
-    params.update(add_dict)
-    return params
-
-def make_factory(cl, **kwargs):
-    def factory():
-        return cl(**kwargs)
-    return factory
-
-
-def create_problem_factories(cmdline_problem_args):   
-    if not cmdline_problem_args:
-        # Default problem factories
-        problem_factories = [
+def create_default_problem_factories():   
+    problem_factories = [
             lambda refinement_level,dt: LidDrivenCavity(ParamDict(refinement_level=refinement_level, dt=dt, T=2.5, mu=1./1000., rho=1.0)),
             lambda refinement_level,dt: Poiseuille2D(ParamDict(refinement_level=refinement_level, dt=dt, T=None, num_periods=0.2)),
             lambda refinement_level,dt: Poiseuille3D(ParamDict(refinement_level=refinement_level, dt=dt, T=None, num_periods=0.2)),
             lambda refinement_level,dt: Womersley2D(ParamDict(refinement_level=refinement_level, dt=dt, T=None, num_periods=1.0)),
             lambda refinement_level,dt: Womersley3D(ParamDict(refinement_level=refinement_level, dt=dt, T=None, num_periods=1.0)), 
             lambda refinement_level,dt: Beltrami(ParamDict(refinement_level=refinement_level, dt=dt, T=1.0)),
-        ]
-    else:
-        # Create factories from what has been parsed from --problems option at command line
-        problem_factories = []
-        for problem_name, params in cmdline_problem_args.items():
-            Problem = eval(problem_name)
-            factory = (lambda Problem, params: lambda refinement_level, dt: Problem(updated_paramdict(params, dict(refinement_level=refinement_level, dt=dt))))(Problem, params)
-            problem_factories.append(factory)
-
-        
+            ]
     return problem_factories
 
+def create_custom_problem_factories(cmdline_problem_args):   
+    # Create factories from what has been parsed from --problems option at command line
+    problem_factories = []
+    for problem_name, params in cmdline_problem_args.items():
+        Problem = eval(problem_name)
+        factory = (lambda Problem, params:
+                       (lambda refinement_level, dt:
+                        Problem(params.update(refinement_level=refinement_level, dt=dt)))
+                   )(Problem, params)
+        problem_factories.append(factory)
+    return problem_factories
 
-def create_scheme_factories(cmdline_scheme_args):
-    if not cmdline_scheme_args:
-        # Default schemes run
-        scheme_factories = [
-            lambda: IPCS(),
-            lambda: Yosida(),
-            lambda: IPCS_Stable(ParamDict(theta=1.0)),
-            lambda: IPCS_Stable(ParamDict(theta=0.5)),
-        ]
+def create_problem_factories(cmdline_problem_args):   
+    if cmdline_problem_args:
+        return create_custom_problem_factories(cmdline_problem_args)
     else:
-        scheme_factories = []
+        return create_default_problem_factories()
 
-        for scheme_name, params in cmdline_scheme_args.items():
-            Scheme = eval(scheme_name)
-            #print params
-            factory = (lambda Scheme, params : lambda: Scheme(params))(Scheme, params)
-            scheme_factories.append(factory)
-        
+def create_default_scheme_factories(cmdline_scheme_args):
+    scheme_factories = [
+        lambda: IPCS(),
+        lambda: Yosida(),
+        lambda: IPCS_Stable(ParamDict(theta=1.0)),
+        lambda: IPCS_Stable(ParamDict(theta=0.5)),
+        ]
     return scheme_factories
 
+def create_custom_scheme_factories(cmdline_scheme_args):
+    scheme_factories = []
+    for scheme_name, params in cmdline_scheme_args.items():
+        Scheme = eval(scheme_name)
+        factory = (lambda Scheme, params:
+                   (lambda: Scheme(params))
+                   )(Scheme, params)
+        scheme_factories.append(factory)
+    return scheme_factories
 
+def create_scheme_factories(cmdline_scheme_args):
+    if cmdline_scheme_args:
+        return create_custom_scheme_factories(cmdline_scheme_args)
+    else:
+        return create_default_scheme_factories()
 
 def pytest_generate_tests(metafunc):
     if metafunc.function.func_name != "test_run_problem":
@@ -124,7 +124,7 @@ def pytest_generate_tests(metafunc):
     else:
         test_run_problem = metafunc
 
-    # Create problem and scheme factoriehe pos
+    # Create problem and scheme factories
     problem_factories = create_problem_factories(metafunc.config.option.problems)
     scheme_factories = create_scheme_factories(metafunc.config.option.schemes)
 
@@ -133,28 +133,24 @@ def pytest_generate_tests(metafunc):
         params = dict(
             refinements = range(5),
             dts = [0.1, 0.05, 0.025, 0.0125, 0.00625],
-        )
+            )
     elif testsize == "fast":
         params = dict(
             refinements = range(3),
             dts = [0.1, 0.05],
-        )
+            )
     elif testsize == "slow":
         params = dict(
             refinements = range(3,5),
             dts = [0.025, 0.0125, 0.00625],
-        )
+            )
     elif testsize == "debug":
         params = dict(
             refinements = [0],
             dts = [0.1]
-        )
-
+            )
 
     test_run_problem.parametrize("scheme_factory", scheme_factories)
     test_run_problem.parametrize("problem_factory", problem_factories)
     test_run_problem.parametrize("refinement_level", params["refinements"])
     test_run_problem.parametrize("dt", params["dts"])
-    
-    
-    
--- tests/validation_pytest/test_scheme_convergence.py
+++ tests/validation_pytest/test_scheme_convergence.py
@@ -14,15 +14,14 @@ import pytest
 class NoOutput():
     def write(self, s):
         pass
-        
-    
+
 def l2norm(val, ref, spaces):
     if isinstance(val, (float, int)):
         err = (val-ref)**2
         ref = abs(ref)**2
     elif isinstance(val, Function):
         degree = val.ufl_element().degree()
-        
+
         # Set degree as 3 higher than that if val if not already set
         if isinstance(ref, Expression):
             if not hasattr(ref, "degree"):
@@ -32,42 +31,39 @@ def l2norm(val, ref, spaces):
                 if not hasattr(ref, "degree"):
                     ref_i.degree = degree+3
             ref = as_vector(ref)
-        
+
         err = assemble(inner(val-ref,val-ref)*dx())
         ref = assemble(inner(ref, ref)*dx(), mesh=val.function_space().mesh())
-    
+
     if ref > 1e-14:
         return sqrt(err/ref)
     else:
         return sqrt(err)
-    
-    
-    
-    
+
 
 class TestConvergence():
-    def test_run_problem(self,problem_factory, scheme_factory, refinement_level, dt):
-        
-        problem = problem_factory(refinement_level,dt)
+    def test_run_problem(self, problem_factory, scheme_factory, refinement_level, dt):
+
+        problem = problem_factory(refinement_level, dt)
         scheme = scheme_factory()
-        
+
         print 
         print "."*100
-        print "** Problem: %16s ** Solver: %16s" %(problem.shortname(), scheme.shortname())
-        print "** Refinement level: %2d ** dt: %.8f" %(refinement_level, dt)
+        print "** Problem: %16s ** Solver: %16s" % (problem.shortname(), scheme.shortname())
+        print "** Refinement level: %2d ** dt: %.8f" % (refinement_level, dt)
         print "**"
-        
+
         pp = NSPostProcessor({"casedir": "test"})
         test_fields = problem.test_fields()
         pp.add_fields(test_fields)
-        
+
         solver = NSSolver(problem, scheme, pp)
-        
+
         # Define variables
         errors = None
         num_dofs = 0
         T = 0
-        
+
         # Disable printing from solve
         # TODO: Move to NSSolver/set option
         original_stdout = sys.stdout
@@ -78,31 +74,33 @@ class TestConvergence():
             ns = solver.solve()
             t2 = time.time()
             T = t2-t1
+
             spaces = ns["spaces"]
             t = float(ns["t"])
-            
-            num_dofs = ns["spaces"].V.dim()+ns["spaces"].Q.dim()
+
             errors = {}
             references = problem.test_references(spaces, t)
-            for i, tf in enumerate(test_fields):
-                val = pp.get(tf.name)
-                ref = references[i]
-                
+            for field, ref in zip(test_fields, references):
+                value = pp.get(field.name)
+
                 # TODO: Use field
-                errors[tf.name] = l2norm(val, ref, spaces)
+                errors[field.name] = l2norm(value, ref, spaces)
+
+            num_dofs = spaces.V.dim()+spaces.Q.dim()
 
         except RuntimeError as re:
             pass
-        
+
         # Enable printing again, and print errors
         sys.stdout = original_stdout
-        print "** dofs: %d Time spent: %f" %(num_dofs , T)
+
+        print "** dofs: %d Time spent: %f" % (num_dofs , T)
         if errors:
             for tfname, err in errors.items():
                 print "**** Fieldname: %20s ** Error: %.8e" %(tfname, err)
         else:
-            print "**** No errors calculated. Solver most likely failed."
-        
+            print "**** No errors calculated. Solver most likely failed
+
 
         # Store solve metadata
         metadata = {}
@@ -114,40 +112,45 @@ class TestConvergence():
         metadata["problem"]["params"] = problem.params
         metadata["num_dofs"] = num_dofs
         metadata["time"] = T
-        
+
         # Find hash from problem and scheme name+parameters
         hash = sha1()
         hash.update(str(metadata["scheme"]))
         hash.update(str(metadata["problem"]))
-        filename = os.path.join("reference", hash.hexdigest()+".db")
-        
+
+        filename = hash.hexdigest()+".db"
+        ref_filename = os.path.join("reference", filename)
+        out_filename = os.path.join("output", filename)
+
         type = pytest.config.option.type
         if type == "update":
+            # Write to reference/
             if not os.path.isdir("reference"):
                 os.mkdir("reference")
 
-            # Write to file
-            f = shelve.open(filename, 'n')
+            f = shelve.open(ref_filename, 'n')
             f["metadata"] = metadata
             f["errors"] = errors
             f.close()
 
         elif type == "changed":
-            # Write to output
+            # Write to output/
             if not os.path.isdir("output"):
                 os.mkdir("output")
-            f = shelve.open(os.path.join("output", hash.hexdigest()+".db"))
+
+            f = shelve.open(out_filename)
             f["metadata"] = metadata
             f["errors"] = errors
             f.close()
-            
+
             # Check against reference
-            assert os.path.isfile(filename), "Unable to find file for case (hash=%s)" %hash
-            ref = shelve.open(filename, 'r')
+            assert os.path.isfile(ref_filename), "Unable to find file for case (hash=%s)" %hash
+            ref = shelve.open(ref_filename, 'r')
             ref_errors = ref["errors"]
-            if not errors:
-                assert not ref_errors, "Unable to compute errors, but errors have been computed in reference"
-            if errors:
-                for key in errors:
-                    assert key in ref_errors, "Unable to find key %s in reference" %key
-                    assert abs(errors[key]-ref_errors[key]) < 1e-14, "Error not matching reference: key=%s error=%f, ref_error=%f" %(key, errors[key], ref_errors[key])
+
+            assert set(errors.keys()) == set(ref_errors.keys()), "Different set of errors computed."
+
+            for key in errors:
+                assert key in ref_errors, "Unable to find key %s in reference" %key
+                assert abs(errors[key]-ref_errors[key]) < 1e-14, "Error not matching reference: key=%s error=%f, ref_error=%f" % (key, errors[key], ref_errors[key])
+
