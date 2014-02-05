c3d29b99102b2de7e11b327dcab07481e348caba
Martin Sandve Alnæs <martinal@simula.no>
2014-02-05 19:03:41 +0100

    Some more refactoring of regression test code.

--- tests/validation_pytest/test_scheme_convergence.py
+++ tests/validation_pytest/test_scheme_convergence.py
@@ -18,7 +18,7 @@ class NoOutput():
     def write(self, s):
         pass
 
-def l2norm(val, ref, spaces):
+def l2norm(val, ref):
     if isinstance(val, (float, int)):
         err = (val-ref)**2
         ref = ref**2
@@ -42,6 +42,21 @@ def l2norm(val, ref, spaces):
 
     return sqrt(err/ref) if ref > 1e-14 else sqrt(err)
 
+def write_data(path, filename, metadata, errors):
+    ref_filename = os.path.join(path, filename)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    f = shelve.open(ref_filename, 'n')
+    f["metadata"] = metadata
+    f["errors"] = errors
+    f.close()
+
+def read_data(path, filename):
+    ref_filename = os.path.join(path, filename)
+    assert os.path.isfile(ref_filename), "Unable to find file %s in %s" % (filename, path)
+    ref = shelve.open(ref_filename, 'r')
+    return ref["metadata"], ref["errors"]
 
 class TestConvergence():
     def test_run_problem(self, problem_factory, scheme_factory, refinement_level, dt):
@@ -62,7 +77,7 @@ class TestConvergence():
         solver = NSSolver(problem, scheme, pp)
 
         # Define variables
-        errors = None
+        errors = {}
         num_dofs = 0
         T = 0
 
@@ -79,31 +94,25 @@ class TestConvergence():
 
             spaces = ns["spaces"]
             t = float(ns["t"])
+            num_dofs = spaces.V.dim()+spaces.Q.dim()
 
-            errors = {}
             references = problem.test_references(spaces, t)
             for field, ref in zip(test_fields, references):
                 value = pp.get(field.name)
-
                 # TODO: Use field
-                errors[field.name] = l2norm(value, ref, spaces)
-
-            num_dofs = spaces.V.dim()+spaces.Q.dim()
+                errors[field.name] = l2norm(value, ref)
 
         except RuntimeError as re:
             print re.message
-            pass
 
         # Enable printing again, and print errors
         sys.stdout = original_stdout
 
         print "** dofs: %d Time spent: %f" % (num_dofs , T)
-        if errors:
-            for tfname, err in errors.items():
-                print "**** Fieldname: %20s ** Error: %.8e" %(tfname, err)
-        else:
+        for tfname, err in errors.items():
+            print "**** Fieldname: %20s ** Error: %.8e" % (tfname, err)
+        if not errors:
             print "**** No errors calculated. Solver most likely failed"
-
 
         # Store solve metadata
         metadata = {}
@@ -115,46 +124,35 @@ class TestConvergence():
         metadata["problem"]["params"] = problem.params
         metadata["num_dofs"] = num_dofs
         metadata["time"] = T
+        # TODO: Why split errors out from metadata? More generic to just place it in there?
+        #       We may want to reuse regression test code in more than one test function so generic would be nice.
+        #metadata["errors"] = errors
 
         # Find hash from problem and scheme name+parameters
         hash = sha1()
         hash.update(str(metadata["scheme"]))
         hash.update(str(metadata["problem"]))
 
-        filename = hash.hexdigest()+".db"
-        ref_filename = os.path.join("reference", filename)
-        out_filename = os.path.join("output", filename)
+        filename = hash.hexdigest() + ".db"
+
+        # FIXME: Drop this update action inside the test, it belongs in a script on the outside.
+        # A typical workflow is this:
+        # - Run tests. This step always writes to output, reads from reference, and reports the difference.
+        # - If user wants to update references, call script to do so, which just copyies output/ over reference/, no rerun of tests necessary.
 
         type = pytest.config.option.type
         if type == "update":
-            # Write to reference/
-            if not os.path.isdir("reference"):
-                os.mkdir("reference")
-
-            f = shelve.open(ref_filename, 'n')
-            f["metadata"] = metadata
-            f["errors"] = errors
-            f.close()
+            write_data("reference", filename, metadata, errors)
 
         elif type == "changed":
-            # Write to output/
-            if not os.path.isdir("output"):
-                os.mkdir("output")
-
-            f = shelve.open(out_filename)
-            f["metadata"] = metadata
-            f["errors"] = errors
-            f.close()
+            write_data("output", filename, metadata, errors)
+            ref_metadata, ref_errors = read_data("reference", filename)
 
             # Check against reference
-            assert os.path.isfile(ref_filename), "Unable to find file for case (hash=%s)" % hash.hexdigest()
-            ref = shelve.open(ref_filename, 'r')
-            ref_errors = ref["errors"]
-
             assert set(errors.keys()) == set(ref_errors.keys()), "Different set of errors computed."
-
             for key in errors:
                 print abs(errors[key]-ref_errors[key])
                 print abs(errors[key]-ref_errors[key])/abs(ref_errors[key])
                 # TODO: Find necessary condition of this check!
-                assert abs(errors[key]-ref_errors[key]) < 1e-6, "Error not matching reference: key=%s error=%e, ref_error=%e (diff=%e)" %(key, errors[key], ref_errors[key], abs(errors[key]-ref_errors[key]))
+                #       Suggestion: FFC has two tolerances, one strict and one acceptable. The acceptable one is enforced while the strict one is just reported. A bit like errors vs warnings.
+                assert abs(errors[key]-ref_errors[key]) < 1e-6, "Error not matching reference: key=%s error=%e, ref_error=%e (diff=%e)" % (key, errors[key], ref_errors[key], abs(errors[key]-ref_errors[key]))
