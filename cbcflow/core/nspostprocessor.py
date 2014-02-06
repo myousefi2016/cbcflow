01832688c6e9371559d52b826be7f030f1f14f0b
Martin Sandve Alnæs <martinal@simula.no>
2014-02-06 20:11:07 +0100

    Disable minify and improve regex to catch single-line comments. Probably not bullet proof but should work unless you more or less deliberately try to break it.

--- cbcflow/core/nspostprocessor.py
+++ cbcflow/core/nspostprocessor.py
@@ -21,7 +21,7 @@ __license__  = "GNU GPL version 3 or any later version"
 
 from cbcflow.core.paramdict import ParamDict
 from cbcflow.core.parameterized import Parameterized
-from cbcflow.utils.core.pyminifier import minify
+#from cbcflow.utils.core.pyminifier import minify
 from cbcflow.utils.common import cbcflow_warning, cbcflow_print, hdf5_link, safe_mkdir, timeit, on_master_process, in_serial
 
 from cbcflow.fields import field_classes, basic_fields, meta_fields, PPField
@@ -171,10 +171,12 @@ class NSPostProcessor(Parameterized):
         s = s.split("'''")
         s = s[0::2]
         s = ''.join(s)
+
         s = s.split('"""')
         s = s[0::2]
         s = ''.join(s)
-        s = minify(s)
+
+        #s = minify(s)
 
         # Get argument names for the compute function
         args = inspect.getargspec(field.compute)[0]
@@ -183,7 +185,7 @@ class NSPostProcessor(Parameterized):
 
         # Read the code for dependencies
         deps = []
-        deps_raw = re.findall(pp_arg+".get\((.+)\)", s)
+        deps_raw = re.findall("^[^\#'\"]*" + "pp" + ".get\(([^)]+)\)", s, flags=re.M)
         for dep in deps_raw:
             # Split into arguments (name, timestep)
             dep = dep.split(',')
