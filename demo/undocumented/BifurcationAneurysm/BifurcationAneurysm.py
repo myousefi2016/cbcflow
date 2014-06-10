0a716cd0a161307327dd9961f127a74ac29885ba
Øyvind Evju <oyvinev@simula.no>
2014-06-10 14:46:47 +0200

    Demonstrates more advanced fields in BifurcationAneurysm demo

--- demo/undocumented/BifurcationAneurysm/BifurcationAneurysm.py
+++ demo/undocumented/BifurcationAneurysm/BifurcationAneurysm.py
@@ -21,7 +21,16 @@ class BifurcationAneurysm(NSProblem):
         NSProblem.__init__(self, params)
 
         mesh = Mesh(files[self.params.refinement_level])
-        self.initialize_geometry(mesh)
+        
+        cell_domains = MeshFunction("size_t", mesh, 3)
+        cell_domains.set_all(0)
+        subdomain = CompiledSubDomain("x[0] > 49.0 && x[0] < 60.0 \
+                                      && x[1] > 37.4 && x[1] < 48.9 \
+                                      && x[2] > 25.2 && x[2] < 40.4")
+
+        subdomain.mark(cell_domains, 1)
+        
+        self.initialize_geometry(mesh, cell_domains=cell_domains)
 
     @classmethod
     def default_params(cls):
@@ -72,35 +81,79 @@ class BifurcationAneurysm(NSProblem):
         bcu, bcp = bcs
         inflow = bcu[0][0]
         for e in inflow: e.set_t(float(t))
+        
+        
 
 
 def main():
     set_log_level(60)
-    dt = 1e-1
-    problem = BifurcationAneurysm(dict(refinement_level=0, dt=dt, T=20*dt))
+    dt = 1e-2
+    problem = BifurcationAneurysm(dict(refinement_level=0, dt=dt, T=2.0))
+    print problem.mesh
     scheme = IPCS_Stable(dict(
         rebuild_prec_frequency = 1,
         u_tent_prec_structure = "same_nonzero_pattern",
-        p_corr_solver_parameters = dict(relative_tolerance=1e-6, absolute_tolerance=1e-6, monitor_convergence=False),
-        u_degree=2,
+        #p_corr_solver_parameters = dict(relative_tolerance=1e-6, absolute_tolerance=1e-6, monitor_convergence=False),
+        u_degree=1,
+        p_degree=1,
         solver_u_tent=("gmres", "additive_schwarz"),
         #solver_u_corr=("cg", "additive_schwarz"),
         solver_u_corr = "WeightedGradient",
         theta=1.0,
         ))
+    
+    parameters["krylov_solver"]["relative_tolerance"] = 1e-15
+    parameters["krylov_solver"]["absolute_tolerance"] = 1e-15
 
     casedir = "results_demo_%s_%s" % (problem.shortname(), scheme.shortname())
-    plot_and_save = dict(plot=True, save=True)
-    fields = [
-        Pressure(plot_and_save),
-        Velocity(plot_and_save),
-        WSS(plot_and_save),
-        ]
-    postproc = NSPostProcessor({"casedir": casedir})
-    postproc.add_fields(fields)
+    plot_and_save = dict(plot=False, save=True, stride_timestep=100)
+    
+    postproc = NSPostProcessor({"casedir": "Results/"})
+    
+    
+    def set_up_fields(problem):
+        T0 = problem.params.T/2.0
+        T1 = problem.params.T
+        fields = []
+        
+        plot = False
+        
+        # Basic fields
+        fields.append(Pressure(dict(plot=plot, save=True, stride_timestep=10)))
+        fields.append(Velocity(dict(plot=plot, save=True, stride_timestep=10)))
+    
+        # On boundary
+        fields.append(WSS(dict(plot=plot, save=True, start_time=0)))
+        fields.append(Boundary("Pressure", dict(plot=plot, save=True)))
+        
+        # Time-integrated fields
+        fields.append(TimeIntegral("WSS", dict(save=True, start_time=T0, end_time=T1)))
+        fields.append(OSI(dict(save=True, start_time=T0, end_time=T1)))
+        
+        # SubFunctions and Restrictions
+        from cbcflow.utils.common.submesh import create_submesh
+        from cbcflow.utils.fields.Slice import Slice
+        
+        submesh = create_submesh(problem.mesh, problem.cell_domains, 1)
+        fields.append(Restrict("Velocity", submesh, dict(save=True)))
+        
+        slicemesh = Slice(problem.mesh, (54.8, 44.0, 33.1), (-0.23, -0.10, 0.97))
+        fields.append(SubFunction("Velocity", slicemesh, dict(save=True)))
+        fields.append(SubFunction("Pressure", slicemesh, dict(save=True)))
+        
+        # Point evaluation
+        fields.append(PointEval("Velocity", ((54.8, 44.0, 33.1), (53.6, 43.6, 37.9)), dict(save=True)))
+        
+        # Derivatives
+        fields.append(TimeDerivative("Pressure", dict(save=True)))
+        
+        return fields
 
+    postproc.add_fields(set_up_fields(problem))
+    
     solver = NSSolver(problem, scheme, postproc, dict(timer_frequency=10))
     solver.solve()
+    
 
 if __name__ == "__main__":
     main()
