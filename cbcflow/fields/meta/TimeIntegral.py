e9ee654ec35f9cca0716a3e97d5f4a84ed4ce140
Øyvind Evju <oyvinev@simula.no>
2014-06-10 14:21:52 +0200

    Made TimeIntegral always integrate over correct integration limits.
    A linear interpolation is done if t0 or t1 is outside integration limits.

--- cbcflow/fields/meta/TimeIntegral.py
+++ cbcflow/fields/meta/TimeIntegral.py
@@ -16,35 +16,99 @@
 # along with CBCFLOW. If not, see <http://www.gnu.org/licenses/>.
 from cbcflow.fields.bases.MetaPPField import MetaPPField
 from dolfin import Function
+EPS = 1e-10
 
 class TimeIntegral(MetaPPField):
+    @classmethod
+    def default_params(cls):
+        params = MetaPPField.default_params()
+        params.update(
+            finalize=True,
+            )
+        return params
+    
     def compute(self, pp, spaces, problem):
-        u1 = pp.get(self.valuename)
-        u0 = pp.get(self.valuename, -1)
-
         t1 = pp.get("t")
         t0 = pp.get("t", -1)
-        dt = t1 - t0
 
+        assert t0 <= self.params.end_time+EPS and t1 >= self.params.start_time-EPS, "Trying to compute integral outside the integration limits!"
+
+        # Get integrand
+        u1 = pp.get(self.valuename)
+        u0 = pp.get(self.valuename, -1)
+        
+        #if u0 == "N/A" or u1 == "N/A":
+        #    import ipdb; ipdb.set_trace()
+        
+        assert u0 != "N/A" and u1 != "N/A"
+        
+        # Interpolate to integration limits, if t1 or t0 is outside
+        if t0 < self.params.start_time:
+            if isinstance(u0, Function): start = u0.vector() + (u1.vector()-u0.vector())/(t1-t0)*(self.params.start_time-t0)
+            elif hasattr(u0, "__iter__"): start = [u0[i]+(u1[i]-u0[i])/(t1-t0)*self.params.start_time-t0]
+            else: start = u0 + (u1-u0)/(t1-t0)*(self.params.start_time-t0)
+            t0 = self.params.start_time
+        else:
+            if isinstance(u0, Function): start = u0.vector()
+            else: start = u0
+            
+        if t1 > self.params.end_time:
+            if isinstance(u0, Function): end = u0.vector() + (u1.vector()-u0.vector())/(t1-t0)*(self.params.end_time-t0)
+            elif hasattr(u0, "__iter__"): end = [u0[i]+(u1[i]-u0[i])/(t1-t0)*self.params.end_time-t0]
+            else: end = u0 + (u1-u0)/(t1-t0)*(self.params.end_time-t0)
+            t1 = self.params.end_time
+        else:
+            if isinstance(u1, Function): end = u1.vector()
+            else: end = u1
+        
+        dt = t1 - t0
+        if dt == 0: dt = 1e-14 # Avoid zero-division
+        
+        # Add to sum
         if isinstance(u0, Function):
             # Create placeholder for sum the first time
             if not hasattr(self, "_sum"):
                 self._sum = Function(u0.function_space())
             # Accumulate using trapezoidal integration
-            self._sum.vector().axpy(dt/2.0, u0.vector()) # FIXME: Validate this, not tested!
-            self._sum.vector().axpy(dt/2.0, u1.vector()) 
+            self._sum.vector().axpy(dt/2.0, start) # FIXME: Validate this, not tested!
+            self._sum.vector().axpy(dt/2.0, end)
+        elif hasattr(u0, "__iter__"):
+            # Create placeholder for sum the first time
+            if not hasattr(self, "_sum"):
+                self._sum = [0.0]*len(u0)
+            
+            # Accumulate using trapezoidal integration
+            for i in range(len(u0)):
+                self._sum[i] += dt/2.0*start[i]
+                self._sum[i] += dt/2.0*end[i]
         else:
             # Create placeholder for sum the first time
             if not hasattr(self, "_sum"):
                 self._sum = 0.0
             # Accumulate using trapezoidal integration
-            self._sum += dt/2.0*u0
-            self._sum += dt/2.0*u1
-            
-        ts = pp.get("timestep")
-        if (t1+dt > self.params.end_time
-            or ts == self.params.end_timestep
-            or t1+dt > problem.params.T):
+            self._sum += dt/2.0*start
+            self._sum += dt/2.0*end
+        
+        # Store bounds for sanity check
+        if not hasattr(self, "T0"):
+            self.T0 = t0
+        self.T1 = t1
+        #print "Integrated %s from %f to %f" %(self.valuename, self.T0, self.T1)
+        
+        if not self.params.finalize:
             return self._sum
         else:
             return None
+    
+    def after_last_compute(self, pp, spaces, problem):
+        if not hasattr(self, "_sum"):
+            return None
+        
+        # Integrate last timestep if integration not completed
+        if self.T1 <= self.params.end_time + EPS:
+            self.compute(pp, spaces, problem)
+        
+        #print "Integrated %s from %f to %f" %(self.valuename, self.T0, self.T1)
+        #print self._sum
+
+        return self._sum
